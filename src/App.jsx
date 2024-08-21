2e9d380b8d121b73fbce81565f9c2baa537ff0dc
FlorenciaNieva <florencianieva930@gmail.com>
2024-08-21 15:21:23 -0300

    I add theme.js file and add chakra provider to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
 import "./App.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import Error from "./components/Error";
-import Home from "./components/Home";
-import AboutMe from "./components/AboutMe";
-import Contact from "./components/Contact";
+import Error from "./pages/Error/index.jsx";
+import Home from "./pages/Home/index.jsx";
+import AboutMe from "./pages/AboutMe/index.jsx";
+import Contact from "./pages/Contact/index.jsx";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="*" element={<Error />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/about-me" element={<AboutMe />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer />
+      <ChakraProvider theme={theme}>
+        <Header />
+        <Routes>
+          <Route path="*" element={<Error />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about-me" element={<AboutMe />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </ChakraProvider>
     </BrowserRouter>
   );
 }
